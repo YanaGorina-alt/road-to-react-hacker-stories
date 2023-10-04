d35c8c996fe138ce4fa9f6df58a49a50a575c3fa
Yana Gorina <yagorina.ne@gmail.com>
2023-10-04 11:26:08 -0700

    Add a Handler Function

--- src/App.js
+++ src/App.js
@@ -39,10 +39,14 @@ function List (){
 }
 // Function Expression:
 let Search = function() {
+
+  const handleChange = (event) => {
+    console.log(event);
+  }
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange}/>
     </div>
   )
 }
