721c95faaf31b6a6b68b5f04eedc236083ec4801
Yana Gorina <yagorina.ne@gmail.com>
2023-10-04 12:31:36 -0700

    Use props, Destructure props

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
 
 import './App.css';
-const list = [
-    {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-    },
-    {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-    },
-    ];
+
 // Function Declaration:
-function List (){
+function List (props){
+  const {title, list} = props; // props Destructuring
   return (
-    <ul>
-        {list.map(function(item){
-          return(
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              </li>
-              );
-        })}
+    <div>
+      <h2>{title}</h2>
+      <ul>
+          {list.map(function(item){
+            return(
+                <Item item={item} />
+                );
+          })}
       </ul>
+    </div>
   );
+}
+
+const Item = ({item}) => { // props Destructuring
+  return(
+    <li key={item.objectID}>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    </li>
+  )
 }
 // Function Expression:
 let Search = function() {
@@ -54,6 +47,25 @@ let Search = function() {
 // Arrow Function:
 const App =()=> {
 
+  const stories = [
+    {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+    },
+    {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+    },
+    ];
+
   return (
 
     <div className="App">
@@ -61,7 +73,7 @@ const App =()=> {
       <h1>My Haker Stories</h1>
       <Search/>
       <hr/>
-      <List/>
+      <List list={stories} title="React Ecosystem"/>
     </div>
     
   );
