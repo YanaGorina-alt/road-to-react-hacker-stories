24ca39b080a7838e692a9dff2d0b71297db8cc0f
Yana Gorina <yagorina.ne@gmail.com>
2024-01-01 19:16:49 -0800

    fetching data from API

--- src/App.js
+++ src/App.js
@@ -2,24 +2,7 @@
 import './App.css';
 import * as React from 'react';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -54,21 +37,9 @@ const storiesReducer = (state, action) => {
   }
   };
   
-
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-  setTimeout(
-  () => resolve({ data: { stories: initialStories } }),
-  2000
-  )
-);
-
-
 // Arrow Function:
 const App =()=> {
   
-  
-
   // Instead of using two hooks separatly we'll make a custom hook
 
   // const [searchTerm, setSearchTerm] = React.useState(
@@ -106,10 +77,12 @@ const App =()=> {
   React.useEffect(() => {
     dispatchStories({type: 'STORIES_FETCH_INIT'});
     
-    getAsyncStories().then(result => {
+    fetch(`${API_ENDPOINT}react`)
+      .then((result) => result.json())
+      .then(result => {
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories,
+        payload: result.hits,
         });
     })
     .catch(()=>
