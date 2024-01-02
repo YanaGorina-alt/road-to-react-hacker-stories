4ddb344d0a261c439c5d2add7abe373dbd5213c7
Yana Gorina <yagorina.ne@gmail.com>
2024-01-01 20:33:20 -0800

    Explicit Data Fetching

--- src/App.js
+++ src/App.js
@@ -73,12 +73,16 @@ const App =()=> {
       data: [],
       isError: false,
       isLoading: false,
-    });  
+    }); 
+  const [url, setUrl] = React.useState(
+      `${API_ENDPOINT}${searchTerm}`
+      ); 
   
     const handleFetchStories = React.useCallback(() => { 
       if (!searchTerm) return;
       dispatchStories({ type: 'STORIES_FETCH_INIT' });
-      fetch(`${API_ENDPOINT}${searchTerm}`)
+
+      fetch(url)
       .then((response) => response.json())
       .then((result) => {
       dispatchStories({
@@ -89,7 +93,7 @@ const App =()=> {
       .catch(() =>
       dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
-      }, [searchTerm]);
+      }, [url]);
       
       
       React.useEffect(() => {
@@ -103,10 +107,14 @@ const App =()=> {
       });
   };
 
-  const handleSearch = (event) => {
-    console.log(event.target.value);
+  const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
-  }
+    };
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+    };
+
 
   return (
 
@@ -117,7 +125,7 @@ const App =()=> {
       <InputWithLabel 
         id='search'
         value={searchTerm}
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
         isFocused
         children
       >
@@ -125,6 +133,14 @@ const App =()=> {
         <strong>Search:</strong>
         {/* children ^^^ */} 
       </InputWithLabel>
+
+      <button
+        type="button"
+        disabled={!searchTerm}
+        onClick={handleSearchSubmit}
+        >
+        Submit
+      </button>
     
       <hr/>
       {stories.isError && <p>Something went wrong...</p>}
